dfcc416792cd0f9be2a7c37acae5f48a4a6d4049
Kamil Żarski <209526@edu.p.lodz.pl>
2019-05-27 22:49:17 +0200

    initial

---
+++ src/main/java/com/jibrus/jibrus/JibrusApplication.java
@@ -0,0 +1,13 @@
+package com.jibrus.jibrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibrusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibrusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jibrus/jibrus/JibrusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jibrus.jibrus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JibrusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
